7bd9e0d54b5aa27686bed14b24e011400f86cc93
Alfonso Menproing <alfonso.menproing@mail.ru>
2015-12-11 23:20:33 +0100

    my training

--- trainer_config.py
+++ trainer_config.py
@@ -1,3 +1,15 @@
 DEFAULT_REST = 30
-monday = [ {"exercise" : "Binarios Secuenciales", "BPM" : 100, "duration" : 5, "N" : 2 }, 
-        {"exercise" : "Decimales Secuenciales", "BPM": 150, "duration" : 5} ]
+first = [
+        {"exercise" : "Decimales Secuenciales", "BPM": 180, "duration" : 60, "N" : 10},  
+        {"exercise" : "Binarios Secuenciales", "BPM" : 170, "duration" : 60, "N" : 10 }
+        ]
+
+second = [
+        {"exercise" : "Binarios Secuenciales", "BPM" : 120, "duration" : 60, "N" : 10 },
+        {"exercise" : "Decimales Secuenciales", "BPM": 130, "duration" : 60, "N" : 10}  
+        ]
+
+third = [
+        {"exercise" : "Decimales Secuenciales", "BPM": 70, "duration" : 60, "N" : 10},  
+        {"exercise" : "Binarios Secuenciales", "BPM" : 70, "duration" : 60, "N" : 10 }
+        ]
