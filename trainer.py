a2e867991347ba6f86a2bf598cbbd146d0cd2a6d
Alfonso Menproing <alfonso.menproing@mail.ru>
2015-12-11 23:16:14 +0100

    clearing screen in trainer, and showing round

--- trainer.py
+++ trainer.py
@@ -9,15 +9,16 @@ def main():
     training = uicurses.get_str()
     if training in dir(trainer_config):
         current_training=eval("trainer_config." + training)
-        uicurses.clear()
         for current in current_training:
+            uicurses.clear()
             uicurses.add_dict(current)
             uicurses.wait_any_key(None)
             if "N" in current:
                 N=current["N"]
             else:
                 N=1
-            for _ in range(N):
+            for n_round in range(1,N+1):
+                uicurses.clear()
                 exercise = table.exercises_list[current["exercise"]](uicurses, current)
                 exercise.run()
                 if "rest" in current:
@@ -25,11 +26,10 @@ def main():
                 else:
                     rest=trainer_config.DEFAULT_REST
                 uicurses.clear()
-                uicurses.add_str_center("RESTING: ", -1,0)
+                uicurses.add_str_center(str(n_round) + ", RESTING: ", -1,0)
                 for i in range(rest,-1,-1):
                     uicurses.add_str_center(str(i),0,0,True)
                     uicurses.wait_any_key(1000)
-                uicurses.clear()
             
     else:
         uicurses.add_str("NOT FOUND")
